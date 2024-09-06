f26c36e37b765230f103f86599490acbbdbdd4d2
jatin2732 <jatinsharma2732@gmail.com>
2024-09-06 16:28:01 +0530

    index

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
 const express = require("express");
-const mongoose=require("mongoose");
-const bodyParser=require('body-parser')
-const userRoute=require("./Routes/UserRoutes.js")
-const adminroutes=require("./Routes/adminroutes.js")
-const cors=require('cors')
-const morgan=require("morgan")
-require("dotenv").config()
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const userRoute = require("./Routes/UserRoutes.js");
+const adminroutes = require("./Routes/adminroutes.js");
+const cors = require('cors');
+const morgan = require("morgan");
+require("dotenv").config();
 
-const app = express()
-app.use(cors())
+const app = express();
 
-app.use(bodyParser.json({extended:true,limit:"5mb"}))
-app.use(bodyParser.urlencoded({extended:true,limit:"5mb"}))
-app.use(express.json({extended:true,limit:"5mb"}))
-app.use(express.urlencoded({extended:true,limit:"5mb"}))
+// Middleware
+app.use(cors());
 
-app.use(morgan("dev"))
-app.use("/user",userRoute) 
-app.use("/admin",adminroutes)
-app.use("/ ", (req, res) => {
-    res.send('server is running  ')
+// Body-parser middleware (for parsing JSON and URL-encoded data)
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "5mb" }));
+
+// Morgan for logging requests
+app.use(morgan("dev"));
+
+// Define routes
+app.use("/user", userRoute); 
+app.use("/admin", adminroutes);
+
+// Root route (responding to the base URL)
+app.get("/", (req, res) => {
+    res.send('Server is running');
 });
 
-mongoose.connect(process.env.mongoDB_URL,{})
+// Connect to MongoDB and start the server
+mongoose.connect(process.env.mongoDB_URL, {})
     .then(() => {
         app.listen(process.env.Port, () => {
-            console.log("server is up and running");
-            console.log("connected to the database");
-        })
-
+            console.log("Server is up and running");
+            console.log("Connected to the database");
+        });
     })
     .catch((err) => {
         console.log(err.message);
-    })
-
-
+    });
